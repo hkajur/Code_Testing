a7de4fe7cbce40a292921ff12cadf860cd45c45c
Christopher A Jordan unknown student <caj9@afsconnect1.njit.edu>
2014-10-02 18:24:54 -0400

    Fix the database login php file

--- BackEnd/login.php
+++ BackEnd/login.php
@@ -5,13 +5,16 @@
         $username = $_POST["username"];
         $password = $_POST["password"];
 
-    
+   	if($username == ''){
+		echo "User Name is Empty";
+	}
         $con = mysql_connect("sql.njit.edu", "caj9", "qEpO163u6") 
                or die("Unsucessful Database Connection");
 
         $selectdb = mysql_select_db("caj9", $con);
 
-        $sql_login = "SELECT * FROM Users WHERE UserName=\'" . urldecode($username) . "\' AND UserPassword=\'" . urldecode($password) . "\'";
+        $sql_login = "SELECT * FROM Users WHERE UserName = '" . urldecode($username) . "' AND UserPassword = '"
+	. urldecode($password) . "'";
 
         $result = mysql_query($sql_login);
 
@@ -21,10 +24,10 @@
 
         $num_rows = mysql_num_rows($result);
 
-        if($num_rows === 1){
-            echo "Success";
+        if($num_rows == 1){
+            echo "{ \"Backend_Login\": \"Success\"";
         } else {
-            echo "Failed";
+            echo "{ \"Backend_Login\": \"Failed\" }";
         }        
     } else {
         die("Error: Didn't post to page");
