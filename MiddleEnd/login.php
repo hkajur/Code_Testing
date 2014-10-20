226afacc5e7af09224eb74a265cb8955e564b206
Harish Kajur <hkajur93@gmail.com>
2014-10-19 21:21:09 -0400

    Fix style css for organization and changed login php files

--- MiddleEnd/login.php
+++ MiddleEnd/login.php
@@ -1,45 +1,16 @@
 <?php
 
+    /*
+     * Function rec_input
+     * Validates the input that I am getting
+     * Checks if there are bad html code
+     */
+
     function rec_input($data){
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
         return $data;
-    }
-
-    function njit_login($username, $password){
-
-        $ch = curl_init();
-        
-        /*
-         * Headers for the request that is being sent
-         */ 
-        
-        $postfields = 'pass=' . urlencode($password) . '&user=' . urlencode($username) . '&uuid=' . urlencode('0xACA021');
-        
-        $URL = "https://cp4.njit.edu/cp/home/login";
-        
-        curl_setopt($ch, CURLOPT_URL, $URL);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13');
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
-
-        $page = curl_exec($ch); // make request
-
-        if(curl_errno($ch)){
-                curl_close($ch);
-                return "";
-        }
-        
-        if(strpos($page, 'Failed') !== false){
-            return "NJIT Failed";
-        } else {
-            return "NJIT Success";
-        }
-        
-        curl_close($ch);    
     }
 
     function backend_login($username, $password){
@@ -48,14 +19,16 @@
 
         $URL = "http://web.njit.edu/~caj9/Code_Testing/BackEnd/login.php";
 
-        $postfields = 'username=' . $username . '&password=' . $password;
+        $postfields = array(
+                      'username' => urlencode($username),
+                      'password' => urlencode($password)
+              );
 
         curl_setopt($ch, CURLOPT_URL, $URL);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13');
-        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POST, count($postfields));
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postfields));
 
         $page = curl_exec($ch); // make request
 
@@ -64,15 +37,6 @@
         }
 
         return $page; 
-
-        if(strpos($page, 'Failed') !== false){
-            return "Backend Login Failed";
-        } else if(strpos($page, 'Success') !== false){
-            return "Backend Login Success";
-        } else {
-            return "Error";
-        }
-        
     }
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -80,33 +44,25 @@
         $username = rec_input($_POST["username"]);
         $password = rec_input($_POST["password"]);
 
-        $result = njit_login($username, $password);
+        $array = array();
 
-        $jsonObj = "{ ";
-
-        if(strpos($result, 'Success') !== false){
-                $jsonObj .= "\"NJIT_Login\": \"Success\"";
-        } else if(strpos($result, 'Failed') !== false){
-                $jsonObj .= "\"NJIT_Login\": \"Failed\"";
-        } else {
-                $jsonObj .= "\"NJIT_Login\": \"Error\"";
-        }
-      
-        $jsonObj .= ", ";
+        $array['NJIT_Login'] = "Failed";
 
         $res = backend_login($username, $password);
-
+        
         if(strpos($res, 'Failed') !== false){
-            $jsonObj .= "\"Backend_Login\": \"Failed\"";
+          $array['Backend_Login'] = "Failed";
         } else if(strpos($res, 'Success') !== false){
-            $jsonObj .= "\"Backend_Login\": \"Success\"";
+          $array['Backend_Login'] = "Success";
         } else {
-            $jsonObj .= "\"Backend_Login\": \"Error\"";
+          $array['Backend_Login'] = "Error";
         } 
 
-        $jsonObj .= " }";
+        $obj = json_decode($res);
 
-        echo $jsonObj;
+        $array["userType"] = $obj->{'userType'};
+
+        echo json_encode($array);
 
     } else {
         die("Didn't post to the page");
