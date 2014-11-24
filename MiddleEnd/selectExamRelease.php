7fe68cf00fc8a78899632fb17a4832163bfd707b
Harish Kajur <hkajur93@gmail.com>
2014-11-24 15:15:03 -0500

    Add new files for middleEnd

---
+++ MiddleEnd/selectExamRelease.php
@@ -0,0 +1,33 @@
+<?php
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+        $userID = $_POST["userID"];
+    
+        $URL = "http://afsaccess1.njit.edu/~vk255/Code_Testing/BackEnd/selectExamsRelease.php"; 
+                
+        $ch = curl_init();
+
+        $postfields = array("userID" => $userID);
+
+        curl_setopt($ch, CURLOPT_URL, $URL);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, count($postfields));
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postfields));
+        
+        $page = curl_exec($ch);
+
+        if(curl_errno($ch)){
+            die(json_encode(array("Error" => curl_error($ch))));
+        }
+    
+        curl_close($ch);
+
+        echo $page;
+        
+    } else {
+        die(json_encode(
+            array("Error" => "Invalid POST Request")));
+    }
+?>
